7ba8b4625e1aacc3bca33a364bcd4ed863fb35b7
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-13 15:29:36 +0300

    Add accessor methods for added instance variables

--- student.rb
+++ student.rb
@@ -2,6 +2,8 @@ require "./person.rb"
 
 class Student < Person
 
+  attr_accessor :classroom
+
   def initialize(classroom, age, name = "Unknown", parent_permission = true)
     super(age = age, name = name, parent_permission = parent_permission)
     @classroom = classroom
--- teacher.rb
+++ teacher.rb
@@ -2,6 +2,8 @@ require "./person.rb"
 
 class Teacher < Person
 
+  attr_accessor :specialization
+
   def initialize(specialization, age, name = "Unknown", parent_permission = true)
     super(age = age, name = name, parent_permission = parent_permission)
     @specialization = specialization
