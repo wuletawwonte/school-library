8142d2554f1b516af329d7f47089260700e2da47
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-13 15:27:59 +0300

    Add Teacher class that inherits from Person class

--- student.rb
+++ student.rb
@@ -2,7 +2,7 @@ require "./person.rb"
 
 class Student < Person
 
-  def initialize(age, name = "Unknown", parent_permission = true, classroom)
+  def initialize(classroom, age, name = "Unknown", parent_permission = true)
     super(age = age, name = name, parent_permission = parent_permission)
     @classroom = classroom
   end
---
+++ teacher.rb
@@ -0,0 +1,14 @@
+require "./person.rb"
+
+class Teacher < Person
+
+  def initialize(specialization, age, name = "Unknown", parent_permission = true)
+    super(age = age, name = name, parent_permission = parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services
+    true
+  end
+
+end
