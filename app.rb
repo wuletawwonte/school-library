7319bf755a9945e40ffc82f7c4202535a695e7ce
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-15 10:43:23 +0300

    Implement add book and show books features

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
-require './capitalize_decorator'
-require './trimmer_decorator'
 require './person'
+require './book'
+require './student'
+require './teacher'
+
+$my_books = []
 
 def show_menu 
   puts "Welcome to School Library App!\n\n"
@@ -11,12 +14,43 @@ def show_menu
   puts "4 - Create a book" 
   puts "5 - Create a rental" 
   puts "6 - List all rentals for a given person id" 
-  puts "7 - Exit" 
+  puts "7 - Exit\n\n" 
+  action(gets.chomp)
 end
 
-def main
-  show_menu()
+def action(choice)
+  case choice
+  when "1"
+    $my_books.each { |x| 
+      print "\nTitle: " 
+      puts x.title 
+      print "Author: "
+      puts x.author      
+    }
+    puts "Press enter to continue ..."
+    gets.chomp
+    show_menu()
+  when "2"
+    puts "You chose 2"
+  when "3"
+    puts "You chose 3"
+  when "4"
+    print "title: "
+    title = gets.chomp
+    print "Author: "
+    author = gets.chomp
+    new_book = Book.new(title, author)
+    $my_books.push(new_book)
+    main()
+  when "7"
+    "Bye ..."
+  else
+    puts "You chose something else"
+  end
+end
 
+def main  
+  show_menu()
 end
 
 main()
