1e9e4f10a1ccd391d3f903db638e8563e2599ef3
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-15 22:52:03 +0300

    Create Ap class to hold the logic of the app

--- app.rb
+++ app.rb
@@ -2,49 +2,56 @@ require './person'
 require './book'
 require './student'
 require './teacher'
+require './rental'
 
-$my_books = []
-$people = []
+class App
 
-def show_menu 
-  puts "Welcome to School Library App!\n\n"
-  puts "Please choose an option by entering a number:"
-  puts "1 - List all books" 
-  puts "2 - List all people" 
-  puts "3 - Create a person" 
-  puts "4 - Create a book" 
-  puts "5 - Create a rental" 
-  puts "6 - List all rentals for a given person id" 
-  puts "7 - Exit\n\n" 
-  action(gets.chomp)
-end
+  def initialize
+    @my_books = []
+    @my_rentals = []
+    @people = []
+  end
 
-def list_books
-  $my_books.each_with_index { |x, index| 
-    puts "#{index}) Title: \"#{x.title}\", Author: #{x.author} " 
-  }
-end
+  def show_menu
+    puts "Welcome to School Library App!\n\n"
+    puts "Please choose an option by entering a number:"
+    puts "1 - List all books" 
+    puts "2 - List all people" 
+    puts "3 - Create a person" 
+    puts "4 - Create a book" 
+    puts "5 - Create a rental" 
+    puts "6 - List all rentals for a given person id" 
+    puts "7 - Exit\n\n" 
+    action(gets.chomp)
+  end
 
-def list_people
-  $people.each_with_index { |x, index|
-    puts "#{index}) [#{x.class.name}] Name: #{x.name}, ID: #{x.id}, Age: #{x.age}"
-  }
-end
+  def list_books
+    @my_books.each_with_index { |x, index| 
+      puts "#{index}) Title: \"#{x.title}\", Author: #{x.author} " 
+    }
+  end
 
+  def list_people
+    @people.each_with_index { |x, index|
+      puts "#{index}) [#{x.class.name}] Name: #{x.name}, ID: #{x.id}, Age: #{x.age}"
+    }
+  end
 
-def action(choice)
-  case choice
-  when "1"
+  def action_list_books
     list_books
     puts "Press enter to continue ..."
     gets.chomp
     show_menu()
-  when "2"
+  end
+
+  def action_list_people
     list_people
     puts "\n\nPress any key to continue"
     gets
     show_menu
-  when "3"
+  end
+
+  def create_person 
     my_permission = true;
     print "Do you want to create a student (1) or a teacher (2)? [Input the number]: "
     person_type = gets.chomp    
@@ -59,7 +66,7 @@ def action(choice)
         my_permission = false
       end
       student = Student.new(age, name, my_permission, nil)
-      $people.push(student)
+      @people.push(student)
     elsif (person_type == "2")
       print "Age: "
       age = gets.chomp
@@ -68,39 +75,57 @@ def action(choice)
       print "Specialization: "
       specialization = gets.chomp
       teacher = Teacher.new(age, name, nil, specialization)
-      $people.push(teacher)
+      @people.push(teacher)
     end
     puts "Person created successfully\n"
     gets
     show_menu
-  when "4"
+  end
+
+  def create_book
     print "Title: "
     title = gets.chomp
     print "Author: "
     author = gets.chomp
     new_book = Book.new(title, author)
-    $my_books.push(new_book)
+    @my_books.push(new_book)
     show_menu
-  when "5"
+  end
+
+  def create_rental
     puts "\nSelect a book from the following list by number"
     list_books
     book_index = gets.chomp
     puts "\nSelect a book from the following list by number"
     list_people
     person_index = gets.chomp
-    print "\n Date: "
+    print "\n Date(yyyy/mm/dd): "
     rental_date = gets.chomp
-
-  when "7"
-    puts "Bye ..."
-    exit
-  else
-    puts "You chose something else"
+    new_rental = Rental.new(rental_date, @my_books[book_index], @people[person_index])
+    @my_rentals.push(new_rental)
+    puts "Rental added successfully"
+    gets
+    show_menu
   end
-end
 
-def main  
-  show_menu()
-end
+  def action(choice)
+    case choice
+    when "1"
+      action_list_books
+    when "2"
+      action_list_people
+    when "3"
+      create_person
+    when "4"
+      create_book
+    when "5"
+      create_rental
+    when "7"
+      puts "Bye ..."
+      exit
+    else
+      puts "You chose something else"
+    end
+  end
 
-main()
+end
---
+++ main.rb
@@ -0,0 +1,8 @@
+require './app'
+
+def main
+  app = App.new()
+  app.show_menu
+end
+
+main
